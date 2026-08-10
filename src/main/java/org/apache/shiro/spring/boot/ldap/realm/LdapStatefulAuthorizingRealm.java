846f965fbf0e2fc448ace3991c9d0ca2fe359c5e
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 17:12:49 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/apache/shiro/spring/boot/ldap/realm/LdapStatefulAuthorizingRealm.java
+++ src/main/java/org/apache/shiro/spring/boot/ldap/realm/LdapStatefulAuthorizingRealm.java
@@ -20,7 +20,7 @@ import org.apache.shiro.spring.boot.ldap.token.LdapLoginToken;
 
 /**
  * Ldap Stateful AuthorizingRealm
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 public class LdapStatefulAuthorizingRealm extends AbstractAuthorizingRealm {
 
